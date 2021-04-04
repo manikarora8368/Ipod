13622b6677b25dadf736eab294887152bdb714b9
Kartik4152 <gaming4152@gmail.com>
2021-04-04 21:37:45 +0530

    test

--- src/home.js
+++ src/home.js
@@ -2,7 +2,7 @@ import React from 'react';
 import HomeGif from './assets/home-main.gif';
 const Home=(props)=>{
     const {activeDiv} = props;
-    // console.log(props);
+    //console.log(props);
     return(
         <div className="home-main">
             <div className="main-left">
